bc3c08ed69c00dec7443afc3a00b556df3930653
CarsonAnde <2100183421@qq.com>
2024-05-12 16:23:54 +0800

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'C/main'.
    
     Changes to be committed:
    	new file:   .vscode/tasks.json
    	modified:   test.c
    	modified:   test.exe

--- test.c
+++ test.c
@@ -4,6 +4,7 @@ int main(){
     printf("Please input a,b:");
     scanf("%d,%d",&a,&b);
     c=a+b;
-    printf("The result is %d",c);
+    printf("The result is %d\n",c);
+    printf("I am Lumia950XLfans!");
     return 0;
 }
