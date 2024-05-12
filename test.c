955f39ea0cd76bbe214688bcb72a12cfc1f225bb
CarsonAnde <2100183421@qq.com>
2024-05-12 17:01:58 +0800

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch main  Your branch is up to date with 'C/main'.  Changes to be committed: 	modified:   test.c 	modified:   test.exe

--- test.c
+++ test.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main(){
     int a,b,c;
-    printf("Please input a,b:");
-    scanf("%d,%d",&a,&b);
-    c=a+b;
+    printf("Please input a:");
+    scanf("%d",&a);
+    c=a*2;
     printf("The result is %d\n",c);
     printf("I am Lumia950XLfans!");
     return 0;
